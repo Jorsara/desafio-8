ee252f2be42205928a7ed5c090b17addbdbb62aa
marianobenamo <mariano.benamo@gmail.com>
2021-08-22 11:09:58 -0300

    Desafio 10

--- index.js
+++ index.js
@@ -1,65 +1,30 @@
 
 const express = require('express');
 const app = express();
-const fs = require('fs');
 const bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
+const api = require('./rutas/api.route');
+const handlebars = require('express-handlebars');
 
 const server = app.listen(8080, () =>{
     console.log('Escuchando en el puerto 8080.');
 });
 
-app.get('/api/productos/listar', async (req, res)=>{    
-    try {
-        const data = await fs.promises.readFile('./productos.json');
-        const json = JSON.parse(data.toString('utf-8'));
-        
-        if(json.length > 0){
-            let productos = {
-                items: json,
-                cantidad: 0
-            }
-            productos.cantidad = productos.items.length;
-            res.send(productos);
-        }else{
-            res.send({error : 'no hay productos cargados'});
-        }        
-    } catch (err) {
-    console.log('Error');
-    }
-});
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
-app.get('/api/productos/listar/:id', async (req, res)=>{    
-    try {
-        const data = await fs.promises.readFile('./productos.json');
-        const json = JSON.parse(data.toString('utf-8')); 
-        const result = json.filter(product => product.id == req.params.id);
+// Configuración de handlebars
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+        defaultLayout: 'index.hbs',
+        layoutsDir: __dirname + '/views/layouts',
+        partialsDir: __dirname + '/views/partials/'
+    })
+);
+app.set('view engine', 'hbs');
+app.set('views', './views');
 
-        if(result.length > 0){
-            res.send(result[0]);
-        }else{
-            res.send({error : 'producto no encontrado'});
-        }
-    } catch (err) {
-    console.log('Error');
-    }
-});
-
-app.post('/api/productos/guardar', async (req, res)=>{            
-    try {
-        const data = await fs.promises.readFile('./productos.json');
-        const json = JSON.parse(data.toString('utf-8')); 
-        json.push({ ...req.body, id: json.length + 1 });
-        try {
-            await fs.promises.writeFile('./productos.json', JSON.stringify(json, null, '\t'));
-            res.send('Producto cargado exitosamente.')
-        } catch (err) {
-            throw new Error(err);
-        }
-    } catch (err) {
-    console.log('Error');
-    }
-});
-
+// Establecer ruta /api
+app.use('/api', api);
 
---
+++ rutas/api.route.js
@@ -0,0 +1,124 @@
+
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const router = express.Router();
+
+app.use(bodyParser.json());
+
+// Vista de productos en tabla
+router.get('/productos/vista', async (req, res)=>{    
+    try {
+        const data = await fs.promises.readFile('./productos.json');
+        const json = JSON.parse(data.toString('utf-8'));        
+        
+        let productos = {
+            items: json,
+            cantidad: 0
+        }
+        productos.cantidad = productos.items.length;
+        res.render('./layouts/index', {productos});          
+    } catch (err) {
+    console.log('Error');
+    }    
+});
+
+// Devuele json de productos
+router.get('/productos/listar', async (req, res)=>{    
+    try {
+        const data = await fs.promises.readFile('./productos.json');
+        const json = JSON.parse(data.toString('utf-8'));
+        
+        if(json.length > 0){
+            let productos = {
+                items: json,
+                cantidad: 0
+            }
+            productos.cantidad = productos.items.length;
+            res.send(productos);
+        }else{
+            res.send({error : 'no hay productos cargados'});
+        }        
+    } catch (err) {
+    console.log('Error');
+    }
+});
+
+// Devuelve producto por id
+router.get('/productos/listar/:id', async (req, res)=>{    
+    try {
+        const data = await fs.promises.readFile('./productos.json');
+        const json = JSON.parse(data.toString('utf-8')); 
+        const result = json.filter(product => product.id == req.params.id);
+
+        if(result.length > 0){
+            res.send(result[0]);
+        }else{
+            res.send({error : 'producto no encontrado'});
+        }
+    } catch (err) {
+    console.log('Error');
+    }
+});
+
+// Guardar producto por POST
+router.post('/productos/guardar', async (req, res)=>{            
+    try {
+        const data = await fs.promises.readFile('./productos.json');
+        const json = JSON.parse(data.toString('utf-8')); 
+        json.push({ ...req.body, id: json.length + 1 });
+        
+        try {
+            await fs.promises.writeFile('./productos.json', JSON.stringify(json, null, '\t'));
+            res.send('Producto cargado exitosamente.')
+        } catch (err) {
+            throw new Error(err);
+        }
+    } catch (err) {
+    console.log('Error');
+    }
+});
+
+// Actualizar producto por PUT
+router.put('/productos/actualizar/:id', async (req, res)=>{            
+    try {
+        const data = await fs.promises.readFile('./productos.json');
+        const json = JSON.parse(data.toString('utf-8')); 
+        let prodActualizado = req.body;
+        prodActualizado.id = req.params.id;
+        let foundIndex = json.findIndex(x => x.id == req.params.id);
+        json[foundIndex] = prodActualizado;
+
+        try {
+            await fs.promises.writeFile('./productos.json', JSON.stringify(json, null, '\t'));
+            res.send(prodActualizado)
+        } catch (err) {
+            throw new Error(err);
+        }
+    } catch (err) {
+    console.log('Error');
+    }
+});
+
+// Eliminar producto por DELETE
+router.delete('/productos/borrar/:id', async (req, res)=>{            
+    try {
+        const data = await fs.promises.readFile('./productos.json');
+        const json = JSON.parse(data.toString('utf-8'));        
+        let foundIndex = json.findIndex(x => x.id == req.params.id);
+        prodEliminado = json[foundIndex];
+        let jsonNuevo = json.filter(e => { return e.id != req.params.id; }); 
+        
+        try {
+            await fs.promises.writeFile('./productos.json', JSON.stringify(jsonNuevo, null, '\t'));
+            res.send(prodEliminado)
+        } catch (err) {
+            throw new Error(err);
+        }
+    } catch (err) {
+    console.log('Error');
+    }
+});
+
+module.exports = router;
